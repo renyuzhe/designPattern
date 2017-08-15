517e6be0cc87cca863c39b88ec22a9df4a0a8cb6
renyu_zhe <yuzhe.ren@definesys.com>
2017-08-15 15:12:44 +0800

    templatePattern

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
+import com.pattern.template.templateMain;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        templateMain temp = new templateMain();
+        temp.templatePattern();
     }
 }
